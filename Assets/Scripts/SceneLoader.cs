6538fc2d539648fbf59e8af4366823ec69938fa0
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-16 22:26:50 +0000

    Pedro imports NWUI Package to simplify the button and menu logic.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void EndScenes() // This method calls the Applicaion Quit() method that quits the game. 
+    {
+        Application.Quit();
+    }
+}
+
+// Application.Quit() documentation: https://docs.unity3d.com/ScriptReference/Application.Quit.html
