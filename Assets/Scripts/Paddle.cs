fff9ff708548131b2e869197b3105541146fafcd
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-18 23:39:39 +0000

    Pedro adds Paddle script to control Paddle position. We can now catch our ball!

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
