8a82b774bcf06a31d46e94d60bb34488fac9b45c
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-21 23:30:19 +0000

    Pedro clamps the paddle, players can only use the paddle within the game screen.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,6 +2,10 @@
 
 public class Paddle : MonoBehaviour
 {
+    // Configuration parameters
+
+    [SerializeField] float minX = 1;
+    [SerializeField] float maxX = 16f;
 
     [SerializeField] float screenWidthInUnits = 16f;
     // Start is called before the first frame update
@@ -13,8 +17,11 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Input.mousePosition.x / Screen.width);
+
         float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
-        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y); // In this line stay where you are.
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
         transform.position = paddlePos;
     }
 }
