eca4f7beb89b77cbef72406d6b71882f5dfd0581
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-22 00:45:14 +0000

    Pedro sticks the ball to the paddle, as an initial position.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    /// <summary>
+    /// Configuration parameters
+    /// </summary>
+    [SerializeField] Paddle paddle1;
+
+    // State
+    Vector2 paddleToBallVector; // Distance between paddle and the ball
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y); // Where paddle is!
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
