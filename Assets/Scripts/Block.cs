f2259403a152baf6a83a3b9f3135f6de84004b1c
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-28 19:44:26 +0000

    Pedro creates Level class with CountBreakableBlocks() method, currently being called in Block class and showing up on Unity Level object as we start the game - We see the number of blocks we have nowgit add .!

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,15 @@ public class Block : MonoBehaviour
     // Configuration parameters
     [SerializeField] AudioClip breakSound;
 
+    // Cached reference
+    Level level; //This is where we cache variable of type Level.
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>(); // We're looking for an object of type Level to access it's public methods!!!
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position); // Create an audio source that plays "here"
