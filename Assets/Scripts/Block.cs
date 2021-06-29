0aee9a7d777823850a2232049c6b7c9703d47aed
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-29 00:02:57 +0000

    Pedro refactors OnCollisionEnter2D for readability.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -16,9 +16,14 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position); // Create an audio source that plays "here"
         Destroy(gameObject);
         level.BlockDestroyed(); // Here we countdown the number of blocks on collision.
-        // Debug.Log(collision.gameObject.name);
+                                // Debug.Log(collision.gameObject.name);
     }
 }
