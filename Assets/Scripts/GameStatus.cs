0d92c446431cdadce63a8648c436303c42e3b1e8
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-29 00:00:07 +0000

    Pedro adds a time scale, players can now chose how fast their game can go.

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    // Parameters
+    [Range(0f, 2f)] [SerializeField] float gameSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
