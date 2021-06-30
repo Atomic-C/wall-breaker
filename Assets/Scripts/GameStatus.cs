1e00a53a5d2b0f9e795e39e6792fe9cc94894bfa
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-06-30 02:26:24 +0000

    Pedro finishes score display over levels 1 and 2. Players can now see their score as they playgit add .!

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
     // Configuration Parameters
     [Range(0f, 2f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsePerBlockDestroyed = 85;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State variables
     [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -18,5 +25,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsePerBlockDestroyed; // currentScore = currentScore + pointsPerBlockDestroyed
+        scoreText.text = currentScore.ToString();
     }
 }
