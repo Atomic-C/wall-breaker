42d050ec0f91aa807666d8dca5411b5c51d1f629
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-09 19:44:23 +0000

    Pedro adds JukeBox, and uses the singleton design pattern approach to keep the same JukeBox over two different scenes, so we have the Start Menu theme playing at level 1.

---
+++ Assets/Scripts/JukeBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class JukeBox : MonoBehaviour
+{
+    private void Awake()
+    {
+        int jukeBoxStatus = GameObject.FindObjectsOfType<JukeBox>().Length;
+
+        if (jukeBoxStatus > 1)
+        {
+            gameObject.SetActive(false); // Fixes bug where null reference exceptions happen.
+            Destroy(gameObject); // If there is more than one object, destroy yourself
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject); // If there isn't more than one object, don't destroy yourself
+        }
+    }
+}
