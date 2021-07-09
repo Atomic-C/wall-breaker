e3be88f9a3188fcfe308503a41f53abb6e0a8832
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-07-09 19:59:33 +0000

    Pedro comments JukeBox class statements, should be similar to GameStatus class. Remove autoplay from Levels 4 and 5.

--- Assets/Scripts/JukeBox.cs
+++ Assets/Scripts/JukeBox.cs
@@ -2,11 +2,11 @@
 
 public class JukeBox : MonoBehaviour
 {
-    private void Awake()
-    {
-        int jukeBoxStatus = GameObject.FindObjectsOfType<JukeBox>().Length;
+    private void Awake() // We apply Singleton design pattern to cache the jukeBoxMusic, so points persist
+    { // There can be only one jukeBoxMusic and it must be the same on every scene.
+        int jukeBoxMusic = GameObject.FindObjectsOfType<JukeBox>().Length;
 
-        if (jukeBoxStatus > 1)
+        if (jukeBoxMusic > 1)
         {
             gameObject.SetActive(false); // Fixes bug where null reference exceptions happen.
             Destroy(gameObject); // If there is more than one object, destroy yourself
